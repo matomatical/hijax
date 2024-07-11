52626de72fcbd227aaff48d16f0496f20ea6e76b
Matthew Farrugia-Roberts <m@far.in.net>
2024-07-11 14:44:29 +1000

    faster with stacking

--- workshop1/eca.py
+++ workshop1/eca.py
@@ -89,30 +89,28 @@ def simulate(
 
     # parse initial state
     init_state = init_state.astype(dtype=jnp.uint8)
-    (width,) = init_state.shape
 
-    # accumulate output into this array
-    # extra width is to implement wraparound with slicing
-    history = jnp.zeros((height, width+2), dtype=jnp.uint8)
+    state = jnp.pad(init_state, 1, mode='wrap')
 
-    # first row
-    history = history.at[0, 1:-1].set(init_state)
-    history = history.at[0, 0].set(init_state[-1])
-    history = history.at[0, -1].set(init_state[0])
-    
+    # accumulate output into this list
+    history = [state]
+
     # remaining rows
     for step in tqdm.trange(1, height):
         # apply rules
-        history = history.at[step, 1:-1].set(rule_table[
-            history[step-1, 0:-2],
-            history[step-1, 1:-1],
-            history[step-1, 2:],
-        ])
-        # sync edges
-        history = history.at[step, 0].set(history[step, -2])
-        history = history.at[step, -1].set(history[step, 1])
+        state = jnp.pad(
+            rule_table[
+                state[0:-2],
+                state[1:-1],
+                state[2:],
+            ],
+            1,
+            mode='wrap',
+        )
+        history.append(state)
 
     # return a view of the array without the width padding
+    history = jnp.stack(history)
     return history[:, 1:-1]
 
 
