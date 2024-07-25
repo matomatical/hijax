03481b8f88de9994d4e21c06a9d75c6bdc0a5518
Matthew Farrugia-Roberts <m@far.in.net>
2024-07-25 15:05:10 +1000

    attempt to use non-jax things in PyTrees (doesnt work)

--- workshop3/mlp.py
+++ workshop3/mlp.py
@@ -50,7 +50,71 @@ import mattplotlib as mp
 # Architecture
 
 
-# TODO
+class LinearLayer(equinox.Module):
+    weight_matrix: Array
+    bias_vector: Array
+
+
+    def __init__(
+        self,
+        key: Key,
+        num_inputs: int,
+        num_outputs: int,
+    ):
+        # Xavier initialisation
+        init_bound = jnp.sqrt(6/(num_inputs+num_outputs))
+        self.weight_matrix = jax.random.uniform(
+            key=key,
+            shape=(num_inputs, num_outputs),
+            minval=-init_bound,
+            maxval=init_bound,
+        )
+
+        # zero initialisation
+        self.bias_vector = jnp.zeros((num_outputs,))
+
+
+    def __call__(
+        self,
+        x: Float[Array, '... num_inputs'],
+    ) -> Float[Array, '... num_outputs']:
+        return x @ self.weight_matrix + self.bias_vector
+
+
+class MLPImageClassifier(equinox.Module):
+    layer1: LinearLayer
+    layer2: LinearLayer
+    activation: None
+
+
+    def __init__(
+        self,
+        key: Key,
+        image_shape: tuple[int, int],
+        num_hidden: int,
+        num_classes: int,
+        activation = jnp.tanh,
+    ):
+        key_layer1, key_layer2 = jax.random.split(key)
+        num_inputs = image_shape[0] * image_shape[1]
+        self.layer1 = LinearLayer(key_layer1, num_inputs, num_hidden)
+        self.layer2 = LinearLayer(key_layer2, num_hidden, num_classes)
+        self.activation = activation
+
+
+    def __call__(
+        self,
+        x: Float[Array, '... image_height image_width'],
+    ) -> Float[Array, '... num_classes']:
+        # flatten image
+        x = einops.rearrange(x, '... h w -> ... (h w)')
+        # apply mlp layers
+        x = self.layer1(x)
+        x = self.activation(x) # jax.nn.relu
+        x = self.layer2(x)
+        # logits -> probability distribution
+        x = jax.nn.softmax(x, axis=-1)
+        return x
 
 
 # # # 
@@ -71,27 +135,67 @@ def main(
 
     # initialise model
     print("initialising model...")
-    print("TODO.")
+    key_init_model, key = jax.random.split(key)
+    model = MLPImageClassifier(
+        key=key_init_model,
+        image_shape=(28, 28),
+        num_hidden=num_hidden,
+        num_classes=10,
+    )
+
+    print(model)
 
 
     print("loading and preprocessing data...")
-    print("TODO.")
+    with jnp.load('mnist.npz') as datafile:
+        x_train = datafile['x_train']
+        x_test = datafile['x_test']
+        y_train = datafile['y_train']
+        y_test = datafile['y_test']
+    x_train, x_test, y_train, y_test = jax.tree.map(
+        jnp.array,
+        (x_train, x_test, y_train, y_test),
+    )
+    x_train, x_test = jax.tree.map(
+        lambda x: x/255,
+        (x_train, x_test),
+    )
 
+    print(vis_digits(
+        digits=x_test[:num_digits_per_visualisation],
+        true_labels=y_test[:num_digits_per_visualisation],
+        pred_labels=model(x_test[:num_digits_per_visualisation]).argmax(axis=-1),
+    ))
 
+    
     print("begin training...")
     losses = []
     accuracies = []
     for step in tqdm.trange(num_steps, dynamic_ncols=True):
         # sample a batch
-        tqdm.tqdm.write("TODO: sample a batch...")
+        key_batch, key = jax.random.split(key)
+        batch = jax.random.choice(
+            key=key_batch,
+            a=y_train.size,
+            shape=(batch_size,),
+            replace=False,
+        )
+        x_batch = x_train[batch]
+        y_batch = y_train[batch]
 
         # compute the batch loss and grad
-        tqdm.tqdm.write("TODO: compute batch loss and grad...")
+        loss, grads = equinox.filter_value_and_grad(cross_entropy)(
+            model,
+            x_batch,
+            y_batch,
+        )
 
         # update model
-        tqdm.tqdm.write("TODO: update model (and optimiser)...")
-
-        break
+        model = jax.tree.map(
+            lambda x, g: x - learning_rate * g,
+            model,
+            grads,
+        )
 
         # track metrics
         losses.append((step, loss))
@@ -124,7 +228,33 @@ def main(
 # Metrics
 
 
-# TODO
+def cross_entropy(
+    model: MLPImageClassifier,
+    x_batch: Float[Array, "b h w"],
+    y_batch: Int[Array, "b"],
+) -> float:
+    """
+    Hx(q, p) = - Sum_i p(i) log q(i)
+    """
+    batch_size, = y_batch.shape
+    pred_prob_all_classes = model(x_batch)          # -> batch_size 10
+    pred_prob_true_class = pred_prob_all_classes[   # advanced indexing
+        jnp.arange(batch_size),                     # for each example
+        y_batch,                                    # select prob of true class
+    ]                                               # -> batch_size
+    log_prob_true_class = jnp.log(pred_prob_true_class)
+    avg_cross_entropy = -jnp.mean(log_prob_true_class)
+    return avg_cross_entropy
+
+
+def accuracy(
+    model: MLPImageClassifier,
+    x_batch: Float[Array, "b h w"],
+    y_batch: Int[Array, "b"],
+) -> float:
+    pred_prob_all_classes = model(x_batch)
+    highest_prob_class = pred_prob_all_classes.argmax(axis=-1)
+    return jnp.mean(y_batch == highest_prob_class)
 
 
 # # # 
