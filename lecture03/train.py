89f76e572f19e35b264491ee89bfa85356a35935
Matthew Farrugia-Roberts <m@far.in.net>
2025-12-18 11:50:36 +0000

    dataclass pytree

--- lecture03/train.py
+++ lecture03/train.py
@@ -12,22 +12,76 @@ Learning objectives:
 * introducing jax.tree, particularly jax.tree.map
 """
 
+import dataclasses
 import time
 import tyro
 import matthewplotlib as mp
-from jaxtyping import Bool, Float, Array
+from jaxtyping import Bool, Float, Array, PRNGKeyArray
+from typing import Self
 
 import jax
 import jax.numpy as jnp
 import einops
 
 
-type Model = tuple[
-    Float[Array, "2 hidden"],
-    Float[Array, "hidden"],
-    Float[Array, "hidden 1"],
-    Float[Array, "1"],
-]
+# type Model = tuple[
+#     Float[Array, "2 hidden"],
+#     Float[Array, "hidden"],
+#     Float[Array, "hidden 1"],
+#     Float[Array, "1"],
+# ]
+
+
+# def forward(
+#     w: Model,
+#     xs: Float[Array, "batch_size 2"],
+# ) -> Float[Array, "batch_size 1"]:
+#     W1, b1, W2, b2 = w
+#     # layer 1
+#     xs = xs @ W1 + b1 # float[batch_size, hidden]
+#     # activation
+#     xs = jax.nn.relu(xs)
+#     # layer 2
+#     logits = xs @ W2 + b2 # float[batch_size, 1]
+#     return logits
+
+
+@jax.tree_util.register_dataclass
+@dataclasses.dataclass
+class Model:
+    W1: Float[Array, "2 hidden"]
+    b1: Float[Array, "hidden"]
+    W2: Float[Array, "hidden 1"]
+    b2: Float[Array, "1"]
+
+    @staticmethod
+    def init(key: PRNGKeyArray, num_hidden: int) -> Self:
+        key_W1, key = jax.random.split(key)
+        W1 = jax.random.normal(
+            key=key_W1,
+            shape=(2, num_hidden),
+        ) / jnp.sqrt(num_hidden)
+        b1 = jnp.zeros(num_hidden)
+        key_W2, key = jax.random.split(key)
+        W2 = jax.random.normal(
+            key=key_W2,
+            shape=(num_hidden, 1),
+        ) / jnp.sqrt(num_hidden)
+        b2 = jnp.zeros(1)
+        return Model(W1=W1, b1=b1, W2=W2, b2=b2)
+
+    def forward(
+        w: Self,
+        xs: Float[Array, "batch_size 2"],
+    ) -> Float[Array, "batch_size 1"]:
+        # layer 1
+        xs = xs @ w.W1 + w.b1 # float[batch_size, hidden]
+        # activation
+        xs = jax.nn.relu(xs)
+        # layer 2
+        logits = xs @ w.W2 + w.b2 # float[batch_size, 1]
+        return logits
+
 
 
 def main(
@@ -67,19 +121,22 @@ def main(
     print(vis_data(xs, ys))
     
     # initialise model
-    key_W1, key = jax.random.split(key)
-    W1 = jax.random.normal(
-        key=key_W1,
-        shape=(2, num_hidden),
-    ) / jnp.sqrt(num_hidden)
-    b1 = jnp.zeros(num_hidden)
-    key_W2, key = jax.random.split(key)
-    W2 = jax.random.normal(
-        key=key_W2,
-        shape=(num_hidden, 1),
-    ) / jnp.sqrt(num_hidden)
-    b2 = jnp.zeros(1)
-    w = (W1, b1, W2, b2)
+    # key_init, key = jax.random.split(key)
+    # key_W1, key_init = jax.random.split(key_init)
+    # W1 = jax.random.normal(
+    #     key=key_W1,
+    #     shape=(2, num_hidden),
+    # ) / jnp.sqrt(num_hidden)
+    # b1 = jnp.zeros(num_hidden)
+    # key_W2, key_init = jax.random.split(key_init)
+    # W2 = jax.random.normal(
+    #     key=key_W2,
+    #     shape=(num_hidden, 1),
+    # ) / jnp.sqrt(num_hidden)
+    # b2 = jnp.zeros(1)
+    # w = (W1, b1, W2, b2)
+    key_init, key = jax.random.split(key)
+    w = Model.init(key=key_init, num_hidden=num_hidden)
     
     # training loop
     plot = vis_model(w, xs, ys, 0)
@@ -106,26 +163,12 @@ def main(
         plots.append(plot)
         time.sleep(0.02)
 
-    mp.save_animation(
-        plots,
-        "../gallery/lecture03.gif",
-        bgcolor="black",
-        fps=50,
-    )
-
-
-def forward(
-    w: Model,
-    xs: Float[Array, "batch_size 2"],
-) -> Float[Array, "batch_size 1"]:
-    W1, b1, W2, b2 = w
-    # layer 1
-    xs = xs @ W1 + b1 # float[batch_size, hidden]
-    # activation
-    xs = jax.nn.relu(xs)
-    # layer 2
-    logits = xs @ W2 + b2 # float[batch_size, 1]
-    return logits
+    # mp.save_animation(
+    #     plots,
+    #     "../gallery/lecture03.gif",
+    #     bgcolor="black",
+    #     fps=50,
+    # )
 
 
 def loss(
@@ -133,7 +176,8 @@ def loss(
     xs: Float[Array, "batch_size 2"],
     ys: Bool[Array, "batch_size"],
 ) -> float:
-    logits = forward(w, xs)[:, 0]
+    # logits = forward(w, xs)[:, 0]
+    logits = w.forward(xs)[:, 0]
     cross_entropies = jnp.logaddexp(0, logits) - ys * logits
     mean_cross_entropy = jnp.mean(cross_entropies)
     return mean_cross_entropy
@@ -164,13 +208,15 @@ def vis_model(
     step: int,
 ) -> mp.plot:
     # compute predictions
-    ys_pred = jax.nn.sigmoid(forward(w, xs)[:, 0])
+    # ys_pred = jax.nn.sigmoid(forward(w, xs)[:, 0])
+    ys_pred = jax.nn.sigmoid(w.forward(xs)[:, 0])
 
     # plot
     return mp.axes(
         mp.dstack2(
             mp.function2(
-                F=lambda xs: jax.nn.sigmoid(forward(w, xs)[:,0]),
+                # F=lambda xs: jax.nn.sigmoid(forward(w, xs)[:,0]),
+                F=lambda xs: jax.nn.sigmoid(w.forward(xs)[:,0]),
                 xrange=(-3,3),
                 yrange=(-3,3),
                 width=40,
